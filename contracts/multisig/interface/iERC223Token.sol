7e40ef473e8ce5a89525cf35f63a44baa0cfdf64
Jaro <jaro@nous.lt>
2018-07-12 22:46:27 +0300

    Moving Multisig out of this repo

--- contracts/multisig/interface/iERC223Token.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.4.18;
-
-
-contract iERC223Token {
-    function transfer(address to, uint value, bytes data) public returns (bool ok);
-    function transferFrom(address from, address to, uint value, bytes data) public returns (bool ok);
-}
